8a28e81dd122efe001ff217d4e2db0a376685e19
Maria <marcasrod3@gmail.com>
2022-02-24 17:30:55 +0100

    Crear rotor y avanzar

--- rotor.py
+++ rotor.py
@@ -8,13 +8,11 @@ class Rotor():
 
     rotor_resultante = []
 
-    # contador_pulsaciones = 0
-
     def __init__(self):
         self.creaRotor()
 
-    # En este método se crea los rotores aleatoriamente. A cada letra del abecedario (en orden) se le asigna otra aleatoria.
-    # Se almacena el par "letra fija - letra aleatoria" en un diccionario
+    # En este método se crean los rotores aleatoriamente. A cada letra del abecedario (en orden) se le asigna otra aleatoria.
+    # Se almacena el par "letra fija - letra aleatoria" en un diccionario y este se añade a una lista, rotor final 
     def creaRotor(self):
         for i in range(len(self.abecedario)):
             posicion_aleatoria = random.randint(0, 26)
@@ -22,6 +20,7 @@ class Rotor():
             par_letras_resultante[self.abecedario[i]] = self.abecedario[posicion_aleatoria]
             self.rotor_resultante.append(par_letras_resultante)
     
+    # De momento realizamos la rotación de la lista (el rotor), considerando el nº de pulsaciones como parámetro de entrada
     def avanzaPosicion(self, pulsaciones):
         for i in range(pulsaciones):
             self.rotor_resultante.insert(0, self.rotor_resultante[-1])
@@ -34,6 +33,6 @@ if __name__ == "__main__":
 
     rotor = Rotor()
     print(rotor)
-    rotor.avanzaPosicion(3)
+    rotor.avanzaPosicion(3) # Probamos rotación
     print(rotor)
 
