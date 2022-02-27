21280c6e338a0ae28ce4dcbc14c9bf27141bd39c
Maria <marcasrod3@gmail.com>
2022-02-27 14:18:04 +0100

    Creando rotor optimizado

--- rotor.py
+++ rotor.py
@@ -11,16 +11,27 @@ class Rotor():
     def __init__(self):
         self.creaRotor()
 
+    '''# Opcion 1: Mia, menos eficiente
     # En este método se crean los rotores aleatoriamente. A cada letra del abecedario (en orden) se le asigna otra aleatoria.
-    # Se almacena el par "letra fija - letra aleatoria" en un diccionario y este se añade a una lista, rotor final 
+    # Se almacena el par "letra fija - letra aleatoria" en una tupla y este se añade a una lista, rotor final. 
     def creaRotor(self):
-        for i in range(len(self.abecedario)):
+        posiciones_aleatorias = []  # Creamos una lista de posiciones aleatorias evitando repeticiones de estas
+        for letra in self.abecedario:
             posicion_aleatoria = random.randint(0, 26)
-            par_letras_resultante = dict()
-            par_letras_resultante[self.abecedario[i]] = self.abecedario[posicion_aleatoria]
-            self.rotor_resultante.append(par_letras_resultante)
+            while posicion_aleatoria in posiciones_aleatorias:
+                posicion_aleatoria = random.randint(0, 26)
+            posiciones_aleatorias.append(posicion_aleatoria)
+            self.rotor_resultante.append((letra, self.abecedario[posicion_aleatoria]))'''
+
+    # Opcion 2: Algo mas eficiente
+    def creaRotor(self):
+        abecedario_auxiliar = list(self.abecedario)
+        for letra in self.abecedario:
+            posicion_aleatoria = random.randint(0, len(abecedario_auxiliar)-1)
+            self.rotor_resultante.append((letra, abecedario_auxiliar[posicion_aleatoria]))
+            abecedario_auxiliar.pop(posicion_aleatoria)
     
-    # De momento realizamos la rotación de la lista (el rotor), considerando el nº de pulsaciones como parámetro de entrada
+    # De momento realizamos la rotación de la lista (el rotor), considerando el nº de pulsaciones como parámetro de entrada.
     def avanzaPosicion(self, pulsaciones):
         for i in range(pulsaciones):
             self.rotor_resultante.insert(0, self.rotor_resultante[-1])
